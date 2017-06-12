0d395539d06ab157070afcf654198884ad66450f
AhmetOguzhanGunoz <oguzhangunoz@gmail.com>
2017-06-12 20:21:38 +0300

    Example of TCP Communication
    This console application is an example of TCP communication between client and server. Client sent messages which are in a .txt folder and server gets the message. This application can be developed to a communication tool or can be referenced to built communication apps.

---
+++ Client/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[1024];
+            try
+            {
+                IPEndPoint ClientIPEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+                Socket ClientSender = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    ClientSender.Connect(ClientIPEP);
+                    Console.WriteLine("Socket connected to {0}", ClientSender.RemoteEndPoint.ToString());
+
+                    string filename = "write location of asd.txt in debug/bin folder";
+
+                    ClientSender.SendFile(filename);
+
+                    byte [] msg = Encoding.ASCII.GetBytes("This is a test <stop>");
+                    int bytesSent = ClientSender.Send(msg);
+
+                    int bytesRec = ClientSender.Receive(bytes);
+                    Console.WriteLine("Echoed test  = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                    ClientSender.Shutdown(SocketShutdown.Both);
+                    ClientSender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("Argument Null Exception: {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("Socket Exception: {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected Exception: {0}", e.ToString());
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        public static string data = null;
+
+        static void Main(string[] args)
+        {
+            byte[] ServerByte = new byte[1024];
+            IPEndPoint ServerIPEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket ServerListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            try
+            {
+                ServerListener.Bind(ServerIPEP);
+                ServerListener.Listen(10);
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connection...");
+                    Socket ServerAccept = ServerListener.Accept();
+                    data = null;
+                    while (true)
+                    {
+                        ServerByte = new byte[1024];
+                        int bytesRec = ServerAccept.Receive(ServerByte);
+                        data += Encoding.ASCII.GetString(ServerByte, 0, bytesRec);
+                        string path = "C:\\Users\\1201020005\\Documents\\Visual Studio 2015\\Projects\\TCP\\Server\\bin\\Debug\\asd.txt";
+                        if (!File.Exists(path))
+                        {
+                            File.Create(path);
+                            TextWriter tw = new StreamWriter(path);
+                            tw.WriteLine(data);
+                            tw.Close();
+
+                            if (data.IndexOf("<stop>") > -1)
+                            {
+                                break;
+                            }
+                        }
+                        else if (File.Exists(path))
+                        {
+                            TextWriter tw = new StreamWriter(path);
+                            tw.WriteLine(data);
+                            tw.Close();
+
+                            if (data.IndexOf("<stop>") > -1)
+                            {
+                                break;
+                            }
+                        }
+
+                        Console.WriteLine("Text received: {0}", data);
+                        byte[] msg = Encoding.ASCII.GetBytes(data);
+                        ServerAccept.Send(msg);
+
+                        ServerAccept.Shutdown(SocketShutdown.Both);
+                        ServerAccept.Close();
+                    }
+
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
